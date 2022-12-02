9092d0c17bd20e7c534a81de190fb390926c8c9a
Mauro Sato <mauro.sato@outlook.com>
2022-12-02 01:39:17 -0300

    logica frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, TextField } from '@mui/material';
+import { useRef, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [signIn, setSignIn] = useState(true);
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [nome, setNome] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
+  const inputEmail = useRef(null);
+  const handleLogin = () => {    
+    axios.post("http://localhost:3000/user/signin", {
+      password: senha,
+      email: email
+    })
+      .then((response) => alert(response.data))      
+  }
+  const handleSignUp = () => {
+    axios.post("http://localhost:3000/user", {
+      password: senha,
+      email: email,
+      firstName: nome,
+      lastName: sobrenome 
+    })
+      .then((response) => alert(response.data))
+  }
+  const handleChange = () => {   
+    setNome("")     
+    setEmail("")
+    setSobrenome("")
+    setSenha("")
+    setSignIn(!signIn) 
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        signIn ? (
+          <>
+            <li style={{ "list-style-type": "none" }}>
+              <ul>
+                <TextField value={email} ref={inputEmail} onChange={(e) => {setEmail(e.target.value)}} id="standard-basic" label="E-mail" variant="standard" />
+              </ul>
+              <ul>
+                <TextField value={senha} type="password" onChange={(e) => {setSenha(e.target.value)}} id="standard-basic" label="Senha" variant="standard" />
+              </ul>
+            </li>
+            <div>
+              <Button onClick={() => handleLogin()} variant="contained">Login</Button>
+              <Button onClick={() => handleChange()} variant="outlined">Sign Up</Button>
+            </div>
+          </>
+        ) : (
+          <>
+            <li style={{ "list-style-type": "none" }}>
+              <ul>
+                <TextField value={nome} onChange={(e) => {setNome(e.target.value)}} id="standard-basic" label="Nome" variant="standard" />
+              </ul>
+              <ul>
+                <TextField value={sobrenome} onChange={(e) => {setSobrenome(e.target.value)}} id="standard-basic" label="Sobrenome" variant="standard" />
+              </ul>
+              <ul>
+                <TextField value={email} ref={inputEmail} onChange={(e) => {setEmail(e.target.value)}} id="standard-basic" label="E-mail" variant="standard" />
+              </ul>
+              <ul>
+                <TextField value={senha} type="password" onChange={(e) => {setSenha(e.target.value)}} id="standard-basic" label="Senha" variant="standard" />
+              </ul>
+            </li>
+            <div>
+              <Button onClick={() => handleChange()} variant="contained">Login</Button>
+              <Button onClick={() => handleSignUp()} variant="outlined">Sign Up</Button>
+            </div>
+          </>
+        )
+      }
     </div>
   );
 }
